d98117e223ed2ad206c680809383e8f01a46f713
Erik Alfvin <erikalfvin@gmail.com>
2020-05-14 12:41:18 -0500

    Formatted, styled book search results

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,14 +3,20 @@ import React from "react";
 function Results(props) {
     if(props.results.data) {
         return (
-            <div>
+            <div className="results">
                 <h2>Results</h2>
                 <ul>
                 {props.results.data.items.map((book, i) => {
                     console.log(book);
                     return (
                         <li key={i}>
-                            {book.volumeInfo.title}
+                            <h4>{book.volumeInfo.title}</h4>
+                            <h5>{"Author(s): " + (book.volumeInfo.authors ? book.volumeInfo.authors.join(", ") : "")}</h5>
+                            <br />
+                            <div className="bookInfo">
+                                <img className="bookImg" height="10%" src={book.volumeInfo.imageLinks.smallThumbnail}/> <br/>
+                                <p className="bookDesc">{book.volumeInfo.description}</p>
+                            </div>
                         </li>
                     )
                 })}
