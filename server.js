aade4735c5f729357cde9b7f70e24b0a3de0ec0b
Erik Alfvin <erikalfvin@gmail.com>
2020-05-16 22:47:58 -0500

    unignored build folder

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const mongoose = require("mongoose");
 const routes = require("./routes/routes");
@@ -9,7 +10,7 @@ app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("app/build"));
+    app.use(express.static(path.join(__dirname + "/build")));
 }
 
 app.use(routes);
